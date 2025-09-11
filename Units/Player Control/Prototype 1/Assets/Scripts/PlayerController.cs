3047908257c8516a48d6abd9b393449567f440ff
04014609 <04014609@sfcollege.edu>
2025-09-11 18:11:19 -0400

    Lesson 1.4 Step 3 complete

--- Units/Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Units/Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,11 +14,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
