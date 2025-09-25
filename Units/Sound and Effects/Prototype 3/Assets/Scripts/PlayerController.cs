deb243006f9ac6835f1ae02ed087834475de2d1c
04014609 <04014609@sfcollege.edu>
2025-09-25 18:06:54 -0400

    Lesson 3.4 Complete

--- Units/Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Units/Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+     public AudioClip jumpSound;
+     public AudioClip crashSound;
+    private AudioSource playerAudio;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
-    private Animator playerAnim;
+   
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
@@ -24,7 +32,9 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
             isOnGround = false;  
-            playerAnim.SetTrigger("Jump_trig"); 
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);  
+            dirtParticle.Stop();
         }
     }
 
@@ -35,12 +45,15 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+             dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();playerAudio.PlayOneShot(crashSound, 1.0f);
+            dirtParticle.Stop();
         }
     }
 }
